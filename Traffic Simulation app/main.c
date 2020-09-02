c613e722915a9909c8866cafe0eb40d638c576bc
Omar Khatib <38046460+OmarKhatib96@users.noreply.github.com>
2020-09-02 12:10:32 +0200

    Update main.c

--- Traffic Simulation app/main.c
+++ Traffic Simulation app/main.c
@@ -16,15 +16,13 @@ int main(){
 
 	srand(time(NULL));
 	clock_t debutProgramme,finProgramme;
- 	float tempsEcoule;
- 
- 	debutProgramme = clock();
+	float tempsEcoule;
+	debutProgramme = clock();
 
 	informationsRoute *informationsRoute1=(informationsRoute*)malloc(sizeof(informationsRoute)*NBR_MAX);
 	informationsRoute *informationsRoute2=(informationsRoute*)malloc(sizeof(informationsRoute)*NBR_MAX);
 	float DebutHeure=debutHeure*3600.0;
 	File*route1=simulationTraffic(DebutHeure,informationsRoute1);
-
 	File *route2=simulationTraffic(DebutHeure,informationsRoute2);
 	int *feu=Feu();
 	GestionTraffic(route1,route2,informationsRoute1,informationsRoute2,feu[3]+feu[5]+feu[4]);
@@ -33,13 +31,12 @@ int main(){
 	printf("\n\nConsulter le fichier stats.txt pour le compte rendu journalier et resultats.txt pour le tableau de bord.\n\n\n");
 	printf("\t\tISMIN EI17 : PROJET REALISE PAR KHATIB OMAR\n");	
 
-	
-
 	finProgramme = clock();
- 
+
 	tempsEcoule = ((double)finProgramme-debutProgramme) / CLOCKS_PER_SEC;
- 	printf("temps d'execution : %.2f secondes\n",tempsEcoule);
+	printf("temps d'execution : %.2f secondes\n",tempsEcoule);
 
 	
-	return 0;}
+	return 0;
+}
 
